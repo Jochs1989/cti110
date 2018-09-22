905e088bb8ebd6db28f6a67e49858aec20bf6a5a
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2018-09-21 20:50:47 -0400

    Add files via upload
    First submission.

---
+++ P2T1_SalesPrediction_JeffreyOchs.py
@@ -0,0 +1,10 @@
+# Predicting profit of sales
+# 09/21/2018
+# CTI-110 P2T1 - Sales Prediction
+# Jeffrey Ochs
+
+totalSales=float(input('Enter the projected sales: '))
+
+profit=totalSales*0.23
+
+print('The profit is $',format(profit,',.2f'))
